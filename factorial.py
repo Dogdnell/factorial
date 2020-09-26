8ddb98591ddd3ceb6919f3a3be77ee65196091e4
unknown <dacosta50@uan.edu.co>
2020-09-26 14:25:41 -0500

    factorial de un numero

---
+++ factorial.py
@@ -0,0 +1,11 @@
+numero=int(input("ingrese un numero: "))
+
+factorial = 1
+
+
+for n in range (1,(numero + 1)):
+    factorial = factorial * n
+
+    
+
+    print("el factorial de {0}es: {1}".format(numero, factorial))
